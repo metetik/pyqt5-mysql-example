39db4ddcc7bf940465e0a29b102b8b37238dc8dc
metetik <m.e.tetik03@gmail.com>
2019-05-08 12:44:24 +0300

    Arayüz geliştirildi.Açılır pencereler eklendi.

--- tabdeneme.py
+++ tabdeneme.py
@@ -37,13 +37,14 @@ class MyTableWidget(QWidget):
         self.tab1.layout.addWidget(self.tabloMenu)
         self.tableWidget = QTableWidget()
         self.tableWidget.setRowCount(len(vti.tedarikci_tablo))
-        self.tableWidget.setColumnCount(3)
-        self.tableWidget.setHorizontalHeaderLabels(["Id","Konum","Firma Adı"])
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setHorizontalHeaderLabels(["Id","Ülke","Şehir","Firma Adı"])
         i = 0
         for j in vti.tedarikci_tablo:
             self.tableWidget.setItem(i,0,QTableWidgetItem(j[0]))
             self.tableWidget.setItem(i,1,QTableWidgetItem(j[1]))
             self.tableWidget.setItem(i,2,QTableWidgetItem(j[2]))
+            self.tableWidget.setItem(i,3,QTableWidgetItem(j[3]))
             i+=1
         self.tab1.layout.addWidget(self.tableWidget)
         self.tabloMenu.currentIndexChanged.connect(self.tablolar)
@@ -51,34 +52,102 @@ class MyTableWidget(QWidget):
         
         # tab2
         self.tab2.layout = QVBoxLayout(self)
-        self.pushButton1 = QPushButton("tab2 button")
-        self.tab2.layout.addWidget(self.pushButton1)
+        self.tab2_butonlar1 = QHBoxLayout(self)
+        self.tab2_butonlar2 = QHBoxLayout(self)
+        
+        self.pushButton1 = QPushButton("Hammadde Satın Al")
+        self.tab2_butonlar1.addWidget(self.pushButton1)
+        self.pushButton1.clicked.connect(self.pencere1_ac)
+        self.pushButton2 = QPushButton("Kimyasal Ürün Üret")
+        self.tab2_butonlar1.addWidget(self.pushButton2)
+        self.pushButton2.clicked.connect(self.pencere2_ac)
+        
+        self.pushButton3 = QPushButton("Yeni Kimyasal Ürün Ekle")
+        self.tab2_butonlar2.addWidget(self.pushButton3)
+        self.pushButton3.clicked.connect(self.pencere3_ac)
+        self.pushButton4 = QPushButton("Yeni Tedarikçi")
+        self.tab2_butonlar2.addWidget(self.pushButton4)
+        self.pushButton4.clicked.connect(self.pencere4_ac)
+        
+        self.bilgi_kutusu = QLabel("Bilgi kutusu1")
+        
+        
+        self.tab2.layout.addLayout(self.tab2_butonlar1)
+        self.tab2.layout.addLayout(self.tab2_butonlar2)
+        self.tab2.layout.addWidget(self.bilgi_kutusu)
         self.tab2.setLayout(self.tab2.layout)
         
         # tab3
         self.tab3.layout = QVBoxLayout(self)
-        self.pushButton1 = QPushButton("tab3 button")
-        self.tab3.layout.addWidget(self.pushButton1)
+        self.tab3_butonlar1 = QHBoxLayout(self)
+        self.tab3_butonlar2 = QHBoxLayout(self)
+        
+        self.pushButton5 = QPushButton("Kimyasal Ürün Sat")
+        self.tab3_butonlar1.addWidget(self.pushButton5)
+        self.pushButton5.clicked.connect(self.pencere5_ac)
+        self.pushButton6 = QPushButton("Kimyasal Ürün Sipariş Et")
+        self.tab3_butonlar1.addWidget(self.pushButton6)
+        self.pushButton6.clicked.connect(self.pencere6_ac)
+        
+        self.pushButton7 = QPushButton("Satışları Göster")
+        self.tab3_butonlar2.addWidget(self.pushButton7)
+        self.pushButton7.clicked.connect(self.pencere7_ac)
+        self.pushButton8 = QPushButton("Yeni Müşteri")
+        self.tab3_butonlar2.addWidget(self.pushButton8)
+        self.pushButton8.clicked.connect(self.pencere8_ac)
+        
+        self.bilgi_kutusu = QLabel("Bilgi kutusu2")
+        
+        self.tab3.layout.addLayout(self.tab3_butonlar1)
+        self.tab3.layout.addLayout(self.tab3_butonlar2)
+        self.tab3.layout.addWidget(self.bilgi_kutusu)
         self.tab3.setLayout(self.tab3.layout)
         
         
         # Add tabs to widget
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
+    
+    def pencere1_ac(self):
+        self.p1 = pencere1()
+        self.p1.show()
+    def pencere2_ac(self):
+        self.p2 = pencere2()
+        self.p2.show()
+    def pencere3_ac(self):
+        self.p3 = pencere3()
+        self.p3.show()
+    def pencere4_ac(self):
+        self.p4 = pencere4()
+        self.p4.show()
+    def pencere5_ac(self):
+        self.p5 = pencere5()
+        self.p5.show()
+    def pencere6_ac(self):
+        self.p6 = pencere6()
+        self.p6.show()
+    def pencere7_ac(self):
+        self.p7 = pencere7()
+        self.p7.show()
+    def pencere8_ac(self):
+        self.p8 = pencere8()
+        self.p8.show()
+        
         
     def tablolar(self,i):
         if i == 0:
             self.tab1.layout.removeWidget(self.tableWidget)
             self.tableWidget = QTableWidget()
             self.tableWidget.setRowCount(len(vti.tedarikci_tablo))
-            self.tableWidget.setColumnCount(3)
-            self.tableWidget.setHorizontalHeaderLabels(["Id","Konum","Firma Adı"])
+            self.tableWidget.setColumnCount(4)
+            self.tableWidget.setHorizontalHeaderLabels(["Id","Ülke","Şehir","Firma Adı"])
             self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)#tablo değiştirilemez
             satirIndex = 0
             for satir in vti.tedarikci_tablo:
                 self.tableWidget.setItem(satirIndex,0,QTableWidgetItem(satir[0]))
                 self.tableWidget.setItem(satirIndex,1,QTableWidgetItem(satir[1]))
                 self.tableWidget.setItem(satirIndex,2,QTableWidgetItem(satir[2]))
+                self.tableWidget.setItem(satirIndex,3,QTableWidgetItem(satir[3]))
                 satirIndex += 1
             self.tab1.layout.addWidget(self.tableWidget)
         
@@ -103,13 +172,14 @@ class MyTableWidget(QWidget):
             self.tab1.layout.removeWidget(self.tableWidget)
             self.tableWidget = QTableWidget()
             self.tableWidget.setRowCount(len(vti.uretici_tablo))
-            self.tableWidget.setColumnCount(2)
-            self.tableWidget.setHorizontalHeaderLabels(["Firma Adı", "Konum"])
+            self.tableWidget.setColumnCount(3)
+            self.tableWidget.setHorizontalHeaderLabels(["Firma Adı", "Ülke", "Şehir"])
             self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)#tablo değiştirilemez
             satirIndex = 0
             for satir in vti.uretici_tablo:
                 self.tableWidget.setItem(satirIndex,0,QTableWidgetItem(satir[0]))
                 self.tableWidget.setItem(satirIndex,1,QTableWidgetItem(satir[1]))
+                self.tableWidget.setItem(satirIndex,2,QTableWidgetItem(satir[2]))
                 satirIndex += 1
             self.tab1.layout.addWidget(self.tableWidget)
             
@@ -135,8 +205,9 @@ class MyTableWidget(QWidget):
             self.tableWidget.setRowCount(len(vti.stok_urun_tablo))
             self.tableWidget.setColumnCount(7)
             self.tableWidget.setHorizontalHeaderLabels(["Id","Kimyasal Ürun","Üretim Tarihi","Raf Ömru","İşçilik Maliyeti","Toplam Maliyet","Satış Fiyati"])
+            self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)#tablo değiştirilemez
             satirIndex = 0
-            for satir in vti.stok_hammadde_tablo:
+            for satir in vti.stok_urun_tablo:
                 self.tableWidget.setItem(satirIndex,0,QTableWidgetItem(satir[0]))
                 self.tableWidget.setItem(satirIndex,1,QTableWidgetItem(satir[1]))
                 self.tableWidget.setItem(satirIndex,2,QTableWidgetItem(str(satir[2])))
@@ -150,14 +221,15 @@ class MyTableWidget(QWidget):
             self.tab1.layout.removeWidget(self.tableWidget)
             self.tableWidget = QTableWidget()
             self.tableWidget.setRowCount(len(vti.musteri_tablo))
-            self.tableWidget.setColumnCount(3)
-            self.tableWidget.setHorizontalHeaderLabels(["Müşteri Id", "Müşteri Adı", "Adres"])
+            self.tableWidget.setColumnCount(4)
+            self.tableWidget.setHorizontalHeaderLabels(["Müşteri Id", "Müşteri Adı", "Ülke", "Şehir"])
             self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)#tablo değiştirilemez
             satirIndex = 0
             for satir in vti.musteri_tablo:
                 self.tableWidget.setItem(satirIndex,0,QTableWidgetItem(satir[0]))
                 self.tableWidget.setItem(satirIndex,1,QTableWidgetItem(satir[1]))
                 self.tableWidget.setItem(satirIndex,2,QTableWidgetItem(satir[2]))
+                self.tableWidget.setItem(satirIndex,3,QTableWidgetItem(satir[3]))
                 satirIndex += 1
             self.tab1.layout.addWidget(self.tableWidget)
             
@@ -180,22 +252,134 @@ class MyTableWidget(QWidget):
             self.tableWidget = QTableWidget()
             self.tableWidget.setRowCount(len(vti.bilesen_tablo))
             self.tableWidget.setColumnCount(4)
-            self.tableWidget.setHorizontalHeaderLabels(["Stok Ürün", "Bileşen 1", "Bileşen 2","Bileşen3"])
+            self.tableWidget.setHorizontalHeaderLabels(["Stok Ürün", "Bileşen 1", "Bileşen 2","Bileşen 3"])
             self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)#tablo değiştirilemez
             satirIndex = 0
             for satir in vti.bilesen_tablo:
                 self.tableWidget.setItem(satirIndex,0,QTableWidgetItem(satir[0]))
                 self.tableWidget.setItem(satirIndex,1,QTableWidgetItem(satir[1]))
-                self.tableWidget.setItem(satirIndex,2,QTableWidgetItem(satir[3]))
+                self.tableWidget.setItem(satirIndex,2,QTableWidgetItem(satir[2]))
                 self.tableWidget.setItem(satirIndex,3,QTableWidgetItem(satir[3]))
                 satirIndex += 1         
             self.tab1.layout.addWidget(self.tableWidget)
-#    @pyqtSlot()
-#    def on_click(self):
-#        print("\n")
-#        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-#            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
+class pencere1(QMainWindow):                           
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hammade Satın Al")
+        
+class pencere2(QMainWindow):                           
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Kimyasal Ürün Üret")
+class pencere3(QMainWindow):                          
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Yeni Kimyasal Ürün Ekle")
+class pencere4(QMainWindow):                           
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Yeni Tedarikçi")
+        self.setFixedSize(400,300)
+        
+        self.label1 = QLabel(self)
+        self.label1.setText("Tedarikçi Id : ")
+        self.label1.move(75,50)
+        self.line1 = QLineEdit(self)
+        self.line1.move(175,50)
+        
+        self.label2 = QLabel(self)
+        self.label2.setText("Tedarikçi Firma Adı : ")
+        self.label2.move(75,90)
+        self.line2 = QLineEdit(self)
+        self.line2.move(175,90)
+        
+        self.label3 = QLabel(self)
+        self.label3.setText("Ülke : ")
+        self.label3.move(75,130)
+        self.line3 = QLineEdit(self)
+        self.line3.move(175,130)
+        
+        self.label4 = QLabel(self)
+        self.label4.setText("Şehir : ")
+        self.label4.move(75,170)
+        self.line4 = QLineEdit(self)
+        self.line4.move(175,170)
+  
+        self.buton = QPushButton(self)
+        self.buton.setText("Tamam")
+        self.buton.setGeometry(150,210,80,30)
+   
+        self.buton.clicked.connect(self.tiklandi)
+    
+    def tiklandi(self):
+        self.tedarikci_id = self.line1.text()
+        self.tedarikci_firma_adi = self.line2.text()
+        self.tedarikci_ulke = self.line3.text()
+        self.tedarikci_sehir = self.line4.text()
+        
+        vti.yeni_tedarikci(self.tedarikci_id,self.tedarikci_firma_adi,self.tedarikci_ulke,self.tedarikci_sehir)
+        self.close()
+        
+class pencere5(QMainWindow):                           
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Kimyasal Ürün Sat")        
+class pencere6(QMainWindow):                          
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Kimyasal Ürün Sipariş Et")        
+class pencere7(QMainWindow):                           
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Satışları Göster")        
+class pencere8(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Yeni Müşteri Ekle")
+        self.setFixedSize(400,300)
+        
+        self.label1 = QLabel(self)
+        self.label1.setText("Müşteri Id : ")
+        self.label1.move(75,50)
+        self.line1 = QLineEdit(self)
+        self.line1.move(175,50)
+        
+        self.label2 = QLabel(self)
+        self.label2.setText("Müşteri Adı : ")
+        self.label2.move(75,90)
+        self.line2 = QLineEdit(self)
+        self.line2.move(175,90)
+        
+        self.label3 = QLabel(self)
+        self.label3.setText("Ülke : ")
+        self.label3.move(75,130)
+        self.line3 = QLineEdit(self)
+        self.line3.move(175,130)
+        
+        self.label4 = QLabel(self)
+        self.label4.setText("Şehir : ")
+        self.label4.move(75,170)
+        self.line4 = QLineEdit(self)
+        self.line4.move(175,170)
+  
+        self.buton = QPushButton(self)
+        self.buton.setText("Tamam")
+        self.buton.setGeometry(150,210,80,30)
+   
+        self.buton.clicked.connect(self.tiklandi)
+    
+    def tiklandi(self):
+        self.musteri_id = self.line1.text()
+        self.musteri_adi = self.line2.text()
+        self.musteri_ulke = self.line3.text()
+        self.musteri_sehir = self.line4.text()
+        
+        vti.yeni_musteri(self.musteri_id,self.musteri_adi,self.musteri_ulke,self.musteri_sehir)
+        self.close()
+        
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
--- veritabani_islemleri.py
+++ veritabani_islemleri.py
@@ -22,18 +22,19 @@ cursor.execute("drop table if exists bilesenler")
 cursor.execute("drop table if exists musteri")
 cursor.execute("drop table if exists musteri_urun")
 
-cursor.execute("create table tedarikci          (id varchar(10), konum varchar(30), firma_adi varchar(20), primary key(id))")
+cursor.execute("create table tedarikci          (id varchar(10), ulke varchar(20), sehir varchar(20), firma_adi varchar(20), primary key(id))")
 cursor.execute("create table tedarikci_hammadde (id varchar(10),tedarikci_id varchar(10) ,miktar int,uretim_tarihi varchar(10),raf_omru int,satis_fiyati int, primary key(id))")
-cursor.execute("create table uretici            (firma_adi varchar(20),konum varchar(30))")
+cursor.execute("create table uretici            (firma_adi varchar(20),ulke varchar(20),sehir varchar(20))")
 cursor.execute("create table stok_hammadde      (id varchar(10),hammadde_ismi varchar(10),alis_maliyeti int,stok_miktar int, primary key(id))")
 cursor.execute("create table stok_urun          (id varchar(10),kimyasal_urun varchar(20),uretim_tarihi varchar(10),raf_omru int,iscilik_maliyeti int, toplam_maliyet int,satis_fiyati int, primary key(id))")
-cursor.execute("create table musteri            (musteri_id varchar(10),musteriAdi varchar(20),adres varchar(50))")
+cursor.execute("create table musteri            (musteri_id varchar(10),musteriAdi varchar(20),ulke varchar (20),sehir varchar(20))")
 cursor.execute("create table musteri_urun       (musteri_id varchar(10),urun varchar(20))")
-cursor.execute("create table bilesenler         (stok_urun_id varchar(10),bilesen1 varchar(10),bilesen2 varchar(10),bilesen3 varchar(10))")
+cursor.execute("create table bilesenler         (kimyasal_urun varchar(10),bilesen1 varchar(10),bilesen2 varchar(10),bilesen3 varchar(10))")
 
-cursor.execute("insert into tedarikci(id,konum,firma_adi) values ('1','konum1','firma1')")
-cursor.execute("insert into tedarikci(id,konum,firma_adi) values ('2','konum2','firma2')")
-cursor.execute("insert into tedarikci(id,konum,firma_adi) values ('3','konum3','firma3')")
+cursor.execute("insert into tedarikci(id,ulke,sehir,firma_adi) values ('1','Rusya','Moskova','firma1')")
+cursor.execute("insert into tedarikci(id,ulke,sehir,firma_adi) values ('2','Cin','Pekin','firma2')")
+cursor.execute("insert into tedarikci(id,ulke,sehir,firma_adi) values ('3','ABD','New York','firma3')")
+cursor.execute("insert into tedarikci(id,ulke,sehir,firma_adi) values ('4','Turkiye','Izmir','firma4')")
 
 cursor.execute("insert into tedarikci_hammadde(id,tedarikci_id,miktar,uretim_tarihi,raf_omru,satis_fiyati) values ('1','1',2,'01012019',2,300)")
 cursor.execute("insert into tedarikci_hammadde(id,tedarikci_id,miktar,uretim_tarihi,raf_omru,satis_fiyati) values ('2','1',8,'01012019',3,200)")
@@ -45,7 +46,7 @@ cursor.execute("insert into tedarikci_hammadde(id,tedarikci_id,miktar,uretim_tar
 cursor.execute("insert into tedarikci_hammadde(id,tedarikci_id,miktar,uretim_tarihi,raf_omru,satis_fiyati) values ('8','3',8,'01012019',4,150)")
 cursor.execute("insert into tedarikci_hammadde(id,tedarikci_id,miktar,uretim_tarihi,raf_omru,satis_fiyati) values ('9','3',1,'01012019',6,200)")
 
-cursor.execute("insert into uretici(firma_adi,konum) values ('Uretici','Istanbul')")
+cursor.execute("insert into uretici(firma_adi,ulke,sehir) values ('Uretici','Turkiye','Istanbul')")
 
 cursor.execute("insert into stok_hammadde(id,hammadde_ismi,alis_maliyeti,stok_miktar) values (1,'C',300,10)")
 
@@ -53,13 +54,14 @@ cursor.execute("insert into stok_urun(id,kimyasal_urun,uretim_tarihi,raf_omru,is
 cursor.execute("insert into stok_urun(id,kimyasal_urun,uretim_tarihi,raf_omru,iscilik_maliyeti,toplam_maliyet,satis_fiyati) values ('2','H2O','01012019',4,250,600,750)")
 cursor.execute("insert into stok_urun(id,kimyasal_urun,uretim_tarihi,raf_omru,iscilik_maliyeti,toplam_maliyet,satis_fiyati) values ('3','NH3','01012019',2,150,500,625)")
 
-cursor.execute("insert into bilesenler(stok_urun_id,bilesen1,bilesen2,bilesen3) values ('1','C','2O',null)")
-cursor.execute("insert into bilesenler(stok_urun_id,bilesen1,bilesen2,bilesen3) values ('2','2H','O',null)")
-cursor.execute("insert into bilesenler(stok_urun_id,bilesen1,bilesen2,bilesen3) values ('3','N','3H',null)")
+cursor.execute("insert into bilesenler(kimyasal_urun,bilesen1,bilesen2,bilesen3) values ('C2O','C','2O',null)")
+cursor.execute("insert into bilesenler(kimyasal_urun,bilesen1,bilesen2,bilesen3) values ('H2O','2H','O',null)")
+cursor.execute("insert into bilesenler(kimyasal_urun,bilesen1,bilesen2,bilesen3) values ('NH3','N','3H',null)")
 
-cursor.execute("insert into musteri(musteri_id,musteriAdi,adres) values ('1','musteri1','xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')")
-cursor.execute("insert into musteri(musteri_id,musteriAdi,adres) values ('2','musteri2','yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')")
-cursor.execute("insert into musteri(musteri_id,musteriAdi,adres) values ('3','musteri3','zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')")
+cursor.execute("insert into musteri(musteri_id,musteriAdi,ulke,sehir) values ('1','musteri1','Japonya','Tokyo')")
+cursor.execute("insert into musteri(musteri_id,musteriAdi,ulke,sehir) values ('2','musteri2','Guney Afrika','Cape Town')")
+cursor.execute("insert into musteri(musteri_id,musteriAdi,ulke,sehir) values ('3','musteri3','Ingiltere','Londra')")
+cursor.execute("insert into musteri(musteri_id,musteriAdi,ulke,sehir) values ('4','musteri4','Turkiye','Ankara')")
 
 cursor.execute("insert into musteri_urun(musteri_id,urun) values ('1','CO2')")
 cursor.execute("insert into musteri_urun(musteri_id,urun) values ('1','H2O')")
@@ -114,6 +116,11 @@ bilesen_tablo = cursor.fetchall()
 for i in bilesen_tablo:
     print(i)
 
+def yeni_tedarikci(tedarikci_id,tedarikci_firma_adi,tedarikci_ulke,tedarikci_sehir):
+    print(tedarikci_id,tedarikci_firma_adi,tedarikci_ulke,tedarikci_sehir)
+
+def yeni_musteri(musteri_id,musteri_adi,musteri_ulke,musteri_sehir):
+    print(musteri_id,musteri_adi,musteri_ulke,musteri_sehir)
 
 #sonuc = cursor.fetchall()
 
